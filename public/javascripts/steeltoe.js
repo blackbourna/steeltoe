7e350b6750a3f58851b7e5aebbf1e7b5b9ba62ef
Jonathan Clem <jonathan@jclem.net>
2012-11-17 13:38:46 -0500

    SteelToe#set

--- public/javascripts/steeltoe.js
+++ public/javascripts/steeltoe.js
@@ -1,25 +1,42 @@
-function SteelToe (object) {
+function SteelToe (object, rootObject) {
+  var rootObject = rootObject || object;
+
   function steelToe (property) {
     if (object && property) {
-      return SteelToe(object[property]);
+      return SteelToe(object[property], rootObject);
     } else {
-      return property ? SteelToe() : object;
+      return property ? SteelToe(undefined, rootObject) : object;
     }
+  }
+
+  steelToe.set = function (traversalChain, value) {
+    var keys = traversalChain.split('.');
+
+    for (var i = 0; i < keys.length; i ++) {
+      if (!steelToe()[keys[i]]) {
+        steelToe()[keys[i]] = {};
+      }
+
+      if (i == keys.length - 1) {
+        steelToe()[keys[i]] = value;
+      }
+
+      steelToe = steelToe(keys[i]);
+    }
+
+    return value;
   }
 
   steelToe.walk = function (traversalChain) {
     if (traversalChain) {
-      var keys = traversalChain.split('.'),
-          returnObject = steelToe;
+      var keys = traversalChain.split('.'), i;
 
-      for (var i = 0; i < keys.length; i ++) {
-        returnObject = returnObject(keys[i])
+      for (i = 0; i < keys.length; i += 1) {
+        steelToe = steelToe(keys[i])
       }
-
-      return returnObject();
-    } else {
-      return steelToe();
     }
+
+    return steelToe();
   };
 
   return steelToe;
--- spec/javascripts/SteelToeSpec.js
+++ spec/javascripts/SteelToeSpec.js
@@ -1,41 +1,107 @@
 describe("SteelToe", function () {
-  var object = { name: { first: 'Jonathan', last: 'Clem' } },
-      steelToe = SteelToe(object);
+  var object, steelToe;
 
-  it("can return its original object", function () {
-    expect(steelToe()).toEqual(object);
+  beforeEach(function () {
+    object = { name: { first: 'Jonathan', last: 'Clem' } };
+    steelToe = new SteelToe(object);
   });
 
-  it("can access root-level keys", function () {
-    expect(steelToe('name')()).toEqual({ first: 'Jonathan', last: 'Clem' })
+  describe("getting values", function () {
+    it("can return its original object", function () {
+      expect(steelToe()).toEqual(object);
+    });
+
+    it("can access root-level keys", function () {
+      expect(steelToe('name')()).toEqual({ first: 'Jonathan', last: 'Clem' })
+    });
+
+    it("returns undefined for root-level keys that are undefined", function () {
+      expect(steelToe('nope')()).not.toBeDefined();
+    });
+
+    it("returns undefined for undefined keys nested in defined keys", function () {
+      expect(steelToe('name')('middleName')()).not.toBeDefined();
+    });
+
+    it("returns undefined for undefined keys nested in undefined keys", function () {
+      expect(steelToe('nope')('noWay')()).not.toBeDefined();
+    });
+
+    describe("#walk", function () {
+      describe("with no traversal chain", function () {
+        it("returns its object", function () {
+          expect(steelToe.walk()).toEqual(object);
+        });
+      });
+
+      describe("with a traversal chain", function () {
+        it("splits a string of keys and walks through the object", function () {
+          expect(steelToe.walk('name.first')).toEqual(steelToe('name')('first')());
+        });
+
+        it("returns undefined properties properly", function () {
+          expect(steelToe.walk('this.is.bad')).not.toBeDefined();
+        });
+      });
+    });
   });
 
-  it("returns undefined for root-level keys that are undefined", function () {
-    expect(steelToe('nope')()).not.toBeDefined();
-  });
+  describe("setting values", function () {
+    describe("for a root-level key", function () {
+      beforeEach(function () {
+        steelToe.set('key', 'value');
+      });
 
-  it("returns undefined for undefined keys nested in defined keys", function () {
-    expect(steelToe('name')('middleName')()).not.toBeDefined();
-  });
-
-  it("returns undefined for undefined keys nested in undefined keys", function () {
-    expect(steelToe('nope')('noWay')()).not.toBeDefined();
-  });
-
-  describe("#walk", function () {
-    describe("with no traversal chain", function () {
-      it("returns its object", function () {
-        expect(steelToe.walk()).toEqual(object);
+      it("should set the key to the given value", function () {
+        expect(steelToe('key')()).toEqual('value');
       });
     });
 
-    describe("with a traversal chain", function () {
-      it("splits a string of keys and walks through the object", function () {
-        expect(steelToe.walk('name.first')).toEqual(steelToe('name')('first')());
+    describe("for an existing key nested in an existing object", function () {
+      beforeEach(function () {
+        steelToe.set('name.first', 'New Name');
       });
 
-      it("returns undefined properties properly", function () {
-        expect(steelToe.walk('this.is.bad')).not.toBeDefined();
+      it("should set the key to the given value", function () {
+        expect(steelToe('name')('first')()).toEqual('New Name');
+      });
+    });
+
+    describe("for a non-existent key nested in an existing object", function () {
+      beforeEach(function () {
+        steelToe.set('name.middle', 'New Name');
+      });
+
+      it("should set the key to the given value", function () {
+        expect(steelToe('name')('middle')()).toEqual('New Name');
+      });
+    });
+
+    describe("for a non-existent key nested in a non-existent object", function () {
+      beforeEach(function () {
+        steelToe.set('info.age', 26);
+      });
+
+      it("should create the non-existent object", function () {
+        expect(steelToe('info')()).toEqual({ age: 26 });
+      });
+
+      it("should set the key to the given value", function () {
+        expect(steelToe('info')('age')()).toEqual(26);
+      });
+    });
+
+    describe("for a non-existent key nested multiple levels into in a non-existent object", function () {
+      beforeEach(function () {
+        steelToe.set('info.birthplace.city', 'Indianapolis');
+      });
+
+      it("creates the non-existent objects", function () {
+        expect(steelToe('info')()).toEqual({ birthplace: { city: 'Indianapolis' } });
+      });
+
+      it("should set the key to the given value", function () {
+        expect(steelToe('info')('birthplace')('city')()).toEqual('Indianapolis');
       });
     });
   });
